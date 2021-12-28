123d42622c1b9414f968d27ae672aebc16bb8b69
Madson Lemos <madsonlemos.es@gmail.com>
2021-12-28 13:38:24 -0400

    feat: upload image platform

--- src/modules/platforms/dtos/ICreatePlatformDTO.ts
+++ src/modules/platforms/dtos/ICreatePlatformDTO.ts
@@ -1,10 +1,10 @@
 interface ICreatePlatformDTO {
     id?: string;
-    name?: string;
+    name: string;
     image?: string;
-    link?: string;
-    abstract?: string;
-    details?: string;
+    link: string;
+    abstract: string;
+    details: string;
 }
 
 export { ICreatePlatformDTO };
